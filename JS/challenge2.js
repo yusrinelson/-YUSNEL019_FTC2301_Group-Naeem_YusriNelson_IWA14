fea4cbad809dfb5fca3691e82e6932fc108817dc
yusrinelson <125978974+yusrinelson@users.noreply.github.com>
2023-04-18 15:05:32 +0200

    fixed indentation

--- JS/challenge2.js
+++ JS/challenge2.js
@@ -1,39 +1,39 @@
 // script.js
 
-function add (a, b) { return a + b }
+function add(a, b) { return a + b }
 
-function multiply (a, b) { return a * b }
+function multiply(a, b) { return a * b }
 
-function internal () {
+function internal() {
     const added = this.add(this.internal.a, this.internal.b)
-	const multiplies = this.multiply(added, this.internal.c)
-    console.log( multiplies)
-   
+    const multiplies = this.multiply(added, this.internal.c)
+    console.log(multiplies)
+
 }
 
 
 // Not allowed to change below this
 
 const example1 = {
-	internal: {
-		a: 2,
-		b: 4,
-		c: 8,
-	},
-	add,
-	multiply,
-  calculate: internal
+    internal: {
+        a: 2,
+        b: 4,
+        c: 8,
+    },
+    add,
+    multiply,
+    calculate: internal
 }
 
 const example2 = {
-	internal: {
-		a: 2,
-		b: 2,
-		c: 3,
-	},
-	add,
-	multiply,
-  calculate: internal
+    internal: {
+        a: 2,
+        b: 2,
+        c: 3,
+    },
+    add,
+    multiply,
+    calculate: internal
 }
 
 example1.calculate()
